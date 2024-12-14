ed7732f9d2c09510b71ac4eb6563b1e0a9c52887
Hojun Kim <meringyee@gmail.com>
2024-12-14 23:46:32 +0900

    ws

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,34 @@
-alert("hi");
+const messageList = document.querySelector("ul"),
+  messageForm = document.getElementById("message"),
+  nicknameForm = document.getElementById("nickname");
+
+const socket = new WebSocket("ws://" + window.location.host);
+
+const makeMessage = (type, payload) => JSON.stringify({ type, payload });
+
+socket.addEventListener("open", () => {
+  console.log("connected to server");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("disconnected from server");
+});
+
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+});
+
+nicknameForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -7,6 +9,32 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use(morgan("dev"));
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
-app.listen(3000, () => console.log("http://localhost:3000"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("connected to browser");
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((s) => {
+          s.send(`${socket.nickname}: ${message.payload}`);
+        });
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+  socket.on("close", () => {
+    console.log("disconnected from client");
+  });
+});
+
+server.listen(3000, () => console.log("http://localhost:3000"));
